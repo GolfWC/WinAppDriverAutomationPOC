704a95f6f0d0b725541d45229277fabafce0e858
WChainan <WChainan@securiport.com>
2025-01-29 10:25:46 -0500

    Code cleanup on ApplicationLoginPage.java and ApplicationLoginStep.java

--- src/main/java/pages/ApplicationLoginPage.java
+++ src/main/java/pages/ApplicationLoginPage.java
@@ -2,18 +2,14 @@ package pages;
 
 
 import Utils.BasePage;
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.windows.WindowsDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
